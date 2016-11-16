f9088765fd0062547682d0ccb5d53e354fd9989b
SKrishna <sreeramk@hotmail.com>
2016-11-16 11:42:56 -0800

    Adding first version of iso creation pipeline

--- iso-creation-pipeline.groovy
+++ iso-creation-pipeline.groovy
@@ -1,71 +1,109 @@
+#!groovy
+
+def update_rhel7_repo() {
+	echo "Running a repo sync with Redhat 7"
+	ws('/home/iso-build-user/scripts'){
+		// Try to register with redhat first
+		try { 
+			sh 'sudo  ./subscribe.sh'
+		}
+		catch(err){ // If registration failed, unregister and re-register
+			sh 'sudo  ./unregister.sh'
+			sh 'sudo  ./subscribe.sh'
+		}
+		// Try to sync
+		try { // sync repo
+			sh 'sudo ./repo_sync.sh'
+		}
+		catch (err) {
+			currentBuildResult = 'FAILURE'
+			sh 'sudo  ./unregister.sh' // unregister before exiting
+			sh 'sudo ./unregister.sh'
+			emailext body: 'Reposync for RHEL-7 failed ', attachLog: true, subject: 'Reposync for RHEL-7 failed', to: MAILING_LIST
+			throw err
+		}
+		// if we got here the repo sync was successful
+		sh 'sudo ./rhel7_repofix.sh' //  Fix issues in repo
+		// Unsubscribe once done
+		sh 'sudo ./unregister.sh'
+	}
+}
+
+def update_rhel6_repo() {
+	echo "Running a repo sync with Redhat 6"
+    try { // Try to register with redhat first
+        sh 'sudo  /home/victorhugo/repos/subscribe.sh'
+    }
+    catch(err){ // If registration failed, unregister and re-register
+		sh 'sudo  /home/victorhugo/repos/unregister.sh'
+        sh 'sudo  /home/victorhugo/repos/subscribe.sh'
+    }
+    // Try to sync
+    try { 
+		sh 'sudo  /home/victorhugo/repos/repoSync-NG.sh'
+    }
+    catch (err) {
+		currentBuildResult = 'FAILURE'
+        sh 'sudo /home/victorhugo/repos/unregister.sh' // unregister before exiting
+        emailext body: 'Reposync for RHEL-6 failed ', attachLog: true, subject: 'Reposync for RHEL-6 failed', to: MAILING_LIST
+		throw err
+    }
+    // if we got here the repo sync was successful
+    sh 'sudo  /home/victorhugo/repos/clean.sh' // Cleanup
+    sh 'sudo /home/victorhugo/repos/repoFix.sh' //  Fix issues in repo
+    // Unsubscribe once done
+    sh 'sudo  /home/victorhugo/repos/unregister.sh'
+}
 
 stage ('rpm sync') {
 
     node {
-        if (RPM_SYNC) {
+        
             echo "Running rpm sync..."
             
             if (PLATFORM == 'RHEL-6' ) {
-                node ('rhel6-iso-build-machine') {
-                    echo "Running a repo sync with Redhat 6"
-                    try { // Try to register with redhat first
-                        sh 'sudo -u root -i /home/victorhugo/repos/subscribe.sh'
-                    }
-                    catch(err){ // If registration failed, unregister and re-register
-                        sh 'sudo -u root -i /home/victorhugo/repos/unregister.sh'
-                        sh 'sudo -u root -i /home/victorhugo/repos/subscribe.sh'
-                    }
-                     // Try to sync
-                    try { 
-                        
-                        sh 'sudo -u root -i /home/victorhugo/repos/repoSync-NG.sh'
-                    }
-                    catch (err) {
-                        currentBuildResult = 'FAILURE'
-                        sh 'sudo -u root -i /home/victorhugo/repos/unregister.sh' // unregister before exiting
-                        emailext body: 'Reposync for RHEL-6 failed ', attachLog: true, subject: 'Reposync for RHEL-6 failed', to: MAILING_LIST
-						throw err
-                    }
-                    // if we got here the repo sync was successful
-                    sh 'sudo -u root -i /home/victorhugo/repos/clean.sh' // Cleanup
-                    sh 'sudo -u root -i /home/victorhugo/repos/repoFix.sh' //  Fix issues in repo
-                    // Unsubscribe once done
-                    sh 'sudo -u root -i /home/victorhugo/repos/unregister.sh'
-                } // end of node
+				if (RPM_SYNC == 'true' ) {
+					node ('rhel6-iso-build-machine') {
+						update_rhel6_repo()
+					} // end of node
+				}
             } // end of rhel6 block
             
             if ( PLATFORM == 'RHEL-7' ) {
-                node ('rhel7-repo') {
-                    echo "Running a repo sync with Redhat 7"
-                    // Try to register with redhat first
-                    try { 
-                        sh 'sudo -u root -i /home/iso-build-user/scripts/subscribe.sh'
-                    }
-                    catch(err){ // If registration failed, unregister and re-register
-                        sh 'sudo -u root -i /home/iso-build-user/scripts/unregister.sh'
-                        sh 'sudo -u root -i /home/iso-build-user/scripts/subscribe.sh'
-                    }
-                    // Try to sync
-                    try { // sync repo
-                        sh 'sudo -u root -i /home/iso-build-user/scripts/repo_sync.sh'
-                    }
-                    catch (err) {
-                        currentBuildResult = 'FAILURE'
-                        sh 'sudo -u root -i /home/iso-build-user/unregister.sh' // unregister before exiting
-                        sh 'sudo -u root -i /home/iso-build-user/scripts/unregister.sh'
-                        emailext body: 'Reposync for RHEL-7 failed ', attachLog: true, subject: 'Reposync for RHEL-7 failed', to: MAILING_LIST
-						throw err
-                    }
-                    // if we got here the repo sync was successful
-                    sh 'sudo -u root -i /home/iso-build-user/scripts/rhel7_repofix.sh' //  Fix issues in repo
-                    // Unsubscribe once done
-                    sh 'sudo -u root -i /home/iso-build-user/scripts/unregister.sh'
-                } // end of node block
+				if ( RPM_SYNC == 'true' ) {
+					node ('rhel7-repo') {
+						update_rhel7_repo()
+					}
+				}
+				node ('rhel7-iso'){
+					ws('/home/iso-build-user/ISO_BUILD'){
+						git url: 'ssh://git@10.0.135.6/platform.git', branch: 'master'
+					}
+					
+					def DIR = '/home/iso-build-user/ISO_BUILD/platform-isos/' + PLATFORM + '/' + PROJECT
+
+					ws("${DIR}") {
+						try {
+							if ( PROJECT == 'sensor' ){
+								// Only for sensor the ask is to append a rhel7 next to the version
+								sh(script: "sudo gmake nuke; sudo gmake iso RELEASE=${VERSION}")
+							}
+							else {
+								sh(script: "sudo gmake nuke; sudo gmake iso VERSION=${VERSION}")
+							}
+						}
+						catch(err){
+							emailext body: 'ISO Creation unsuccessful!', attachLog: true, subject: 'ISO Creation unSuccessfull', to: MAILING_LIST
+							currentBuild.result = 'FAILURE'
+						}
+					} // ws block
+				}
             } // end of rhel7 block
-            
-        } // end of if rpm-sync block
+   
     } // end of node
 } // end of rpm sync stage
+
+
 
 // Email success
 node {
