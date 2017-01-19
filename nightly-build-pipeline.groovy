ec4fda017683cb5162e12329f9eebe20648344cb
SKrishna <sreeramk@hotmail.com>
2017-01-18 21:57:13 -0800

    Adding the one with 6 rest api suites in one block

--- nightly-build-pipeline.groovy
+++ nightly-build-pipeline.groovy
@@ -21,6 +21,13 @@ def run_suite(suite, env) {
 	try {
 		sh './gradlew -Dreporting=' + REPORTING + ' -DbuildLogUrl=BUILD_URL/console -DpipelineName=Intellego-CI-Coded-Pipeline -Dsuite=resources/suites/' + suite + '.xml -Denv=resources/config/' + env + ' run | tee ' + suite + '.log 2>&1'
 		sh 'cp build/reports/tests/emailable-report.html   build/reports/tests/' + suite+ '.html'
+		//def exists = fileExists 'build/reports/tests/' + suite + '.html '
+		//	if (exists) {
+		//		sh 'cp build/reports/tests/' + suite + '.html ' + TMPDIR
+		//	}
+        //sh 'cp ' + suite + '.log ' + TMPDIR
+		
+		
         //sh 'zip -j ' + TMPDIR + '/level1.zip build/reports/tests/level1.html'
         sh 'echo "<br>" >> ' + TMPDIR + '/Summary.HTML'
         sh 'echo "--------------" >> ' + TMPDIR + '/Summary.HTML'
@@ -29,15 +36,18 @@ def run_suite(suite, env) {
         sh 'echo "<br>" >> ' + TMPDIR + '/Summary.HTML'
         sh 'echo "--------------" >> ' + TMPDIR + '/Summary.HTML'
         sh 'echo "<br>" >> ' + TMPDIR + '/Summary.HTML'
-        //sh 'grep failed build/reports/tests/testng-results.xml | head -1 | sed -e "s/<//" -e "s/>//" -e "s/testng-results //" >> ' + TMPDIR + '/Summary.HTML'
-        sh 'grep failed build/reports/tests/testng-results.xml | head -1 | sed -e "s/<//" -e "s/>//" -e "s/testng-results //" -e "s+failed+<b><font color=red>failed</font></b>+" -e "s+passed+<b><font color=green>passed</font></b>+" >> ' + TMPDIR + '/Summary.HTML'
+        sh 'grep failed build/reports/tests/testng-results.xml | head -1 | sed -e "s/<//" -e "s/>//" -e "s/testng-results //" >> ' + TMPDIR + '/Summary.HTML'
+        //sh 'grep failed build/reports/tests/testng-results.xml | head -1 | sed -e "s/<//" -e "s/>//" -e "s/testng-results //" -e "s+failed+<b><font color=red>failed</font></b>+" -e "s+passed+<b><font color=green>passed</font></b>+" >> ' + TMPDIR + '/Summary.HTML'
         sh ('grep -wq \"SUCCESSFUL\" ' + suite + '.log ; echo $?')
     } // try block closing
     catch(err) {
         currentBuild.result = 'SUCCESS' // If not set to SUCCESS, the pipeline interprets to an entire failed build
         //TESTS_FAILED++  // increment the count of tests failed
-        echo "False. Successful not found.  need to copy"
+		//sh 'echo No. of tests failed: ' + TESTS_FAILED
+		echo "False. Successful not found.  need to copy"
         sh 'cp ' + suite + '.log build/reports/tests/' + suite +'.html ' + TMPDIR
+        
+		
     } // catch block closing
 
 } // end of def run_suite
@@ -78,31 +88,32 @@ stage ('Build Intellego binary'){
 		else {
 			echo " ******** Building Intellego Binary *********"
 			node ('intellego-build-machine') {
-				try {
+				
 					ws('/home/support/intellego') {
 						echo "Checking out code..."
 						git url: 'ssh://git@10.0.135.6/intellego.git', branch: INTELLEGO_CODE_BRANCH
 					}
+				
+				// Cleanup old binary
+				sh 'sudo rm -rf /home/support/bin/REL_' + INTELLEGO_VERSION + '/root/*'
 
-					// Cleanup old binary
-					sh 'sudo rm -rf /home/support/bin/REL_' + INTELLEGO_VERSION + '/root/*'
-
-					// Generate name for the new BINARY based on timestamp
-					BINARY = INTELLEGO_VERSION + '.' + INTELLEGO_CODE_BRANCH + '.' + env.BUILD_TIMESTAMP
-
+				// Generate name for the new BINARY based on timestamp
+				BINARY = INTELLEGO_VERSION + '.' + INTELLEGO_CODE_BRANCH + '.' + env.BUILD_TIMESTAMP
+                
+                try {
 					ws('/home/support/intellego/build_tool') {
 						sh 'sudo ./build-intellego.sh ' + BINARY
 					}
-
-					def DIR = '/home/support/bin/REL_' + INTELLEGO_VERSION + '/root/' + BINARY
-					ws("${DIR}") {
-						archive '*.bin'
-					}
-				}	 // try block
-
-				catch(err){
+                }
+                catch(err){
 					// Email in case the build failed
-					emailext body: 'BUILD_URL = ' + env.BUILD_URL + '/consoleFull', subject: 'Nightly coded pipeline build has failed! ', to: MAILING_LIST
+					emailext body: 'BUILD_URL = ' + env.BUILD_URL + '/consoleFull', subject: 'Nightly coded pipeline: Binary build failed! ', to: MAILING_LIST
+				}
+				
+				echo "Archiving the binary..."
+				def DIR = '/home/support/bin/REL_' + INTELLEGO_VERSION + '/root/' + BINARY
+				ws("${DIR}") {
+					archive '*.bin'
 				}
 			}// node
 		} // end of else block
@@ -112,14 +123,14 @@ stage ('Build Intellego binary'){
 stage ('Copy Binary to Nodes'){
 
 	if ( ONLY_RUN_TESTS == 'false' ) {
-
+	
 		def COPY_BINARY='sudo rm -f /SS8/SS8_Intellego.bin; sudo mv *.bin /SS8/SS8_Intellego.bin; sudo chmod 775 /SS8/SS8_Intellego.bin'
 
 		parallel 'Node 131': {
 			node('10.0.158.131') {
 				def exists = fileExists '*.bin'
 				if (exists) {
-					sh 'sudo rm *.bin'
+					sh 'sudo rm -f *.bin'
 				}
 				unarchive mapping: ['*.bin' : '.']
 				sh COPY_BINARY
@@ -129,7 +140,7 @@ stage ('Copy Binary to Nodes'){
 			node('10.0.158.132') {
 				def exists = fileExists '*.bin'
 				if (exists) {
-					sh 'sudo rm *.bin'
+					sh 'sudo rm -f *.bin'
 				}
 				unarchive mapping: ['*.bin' : '.']
 				sh COPY_BINARY
@@ -139,7 +150,7 @@ stage ('Copy Binary to Nodes'){
 			node('10.0.158.134') {
 				def exists = fileExists '*.bin'
 				if (exists) {
-					sh 'sudo rm *.bin'
+					sh 'sudo rm -f *.bin'
 				}
 				unarchive mapping: ['*.bin' : '.']
 				sh COPY_BINARY
@@ -150,7 +161,7 @@ stage ('Copy Binary to Nodes'){
 			node('10.0.158.147') {
 				def exists = fileExists '*.bin'
 				if (exists) {
-					sh 'sudo rm *.bin'
+					sh 'sudo rm -f *.bin'
 				}
 				unarchive mapping: ['*.bin' : '.']
 				sh COPY_BINARY
@@ -161,7 +172,7 @@ stage ('Copy Binary to Nodes'){
 			node('10.0.158.148') {
 				def exists = fileExists '*.bin'
 				if (exists) {
-					sh 'sudo rm *.bin'
+					sh 'sudo rm -f *.bin'
 				}
 				unarchive mapping: ['*.bin' : '.']
 				sh COPY_BINARY
@@ -171,7 +182,7 @@ stage ('Copy Binary to Nodes'){
 			node('10.0.158.151') {
 				def exists = fileExists '*.bin'
 				if (exists) {
-					sh 'sudo rm *.bin'
+					sh 'sudo rm -f *.bin'
 				}
 				unarchive mapping: ['*.bin' : '.']
 				sh COPY_BINARY
@@ -182,7 +193,7 @@ stage ('Copy Binary to Nodes'){
 			node('10.0.158.161') {
 				def exists = fileExists '*.bin'
 				if (exists) {
-					sh 'sudo rm *.bin'
+					sh 'sudo rm -f *.bin'
 				}
 				unarchive mapping: ['*.bin' : '.']
 				sh COPY_BINARY
@@ -193,7 +204,7 @@ stage ('Copy Binary to Nodes'){
 			node('10.0.158.152') {
 				def exists = fileExists '*.bin'
 				if (exists) {
-					sh 'sudo rm *.bin'
+					sh 'sudo rm -f *.bin'
 				}
 				unarchive mapping: ['*.bin' : '.']
 				sh COPY_BINARY
@@ -214,7 +225,7 @@ stage ('Install and Test') {
 
 		if ( ONLY_RUN_TESTS == 'false' ) {
 		
-		    timeout(time:1, unit:'HOURS') {
+		    timeout(time:3, unit:'HOURS') {
 			    try {
 					//Install Intellego DPE on Node 134
 					node('10.0.158.134') {
@@ -262,13 +273,6 @@ stage ('Install and Test') {
 				run_suite ( 'level1_tests', 'qa-at-158-148.yaml' )
 			}
 
-			if ( run_level2== 'true' ) {
-				run_suite ( 'level2_tests', 'qa-at-158-148.yaml' )
-			}
-
-			if ( run_kddi== 'true' ) {
-				run_suite ( 'kddi_tests', 'qa-at-158-148.yaml' )
-			}
 		} // End of node jenkins-slave block
 
 	}, // end of 134-148 block
@@ -277,7 +281,7 @@ stage ('Install and Test') {
 
 		// Install Intellego DPE on Node 152'
 		if ( ONLY_RUN_TESTS == 'false' ) {
-			timeout(time:1, unit:'HOURS') {
+			timeout(time:3, unit:'HOURS') {
 				try {
 					node('10.0.158.152') {
 						sh NTPDATE
@@ -325,7 +329,14 @@ stage ('Install and Test') {
 			if ( run_regression == 'true' ) {
 				run_suite ( 'regression_tests', 'qa-at-158-151.yaml' )
 			}
+			
+			if ( run_level2== 'true' ) {
+				run_suite ( 'level2_tests', 'qa-at-158-151.yaml' )
+			}
 
+			if ( run_kddi== 'true' ) {
+				run_suite ( 'kddi_tests', 'qa-at-158-151.yaml' )
+			}
 
 		}//node
 
@@ -334,7 +345,7 @@ stage ('Install and Test') {
 	'131-132': {
 
 		if ( ONLY_RUN_TESTS == 'false' ) {
-			timeout(time:1, unit:'HOURS') {
+			timeout(time:3, unit:'HOURS') {
 				try {
 					//'Install Intellego DPE on Node 132'
 					node('10.0.158.132') {
@@ -392,7 +403,7 @@ stage ('Install and Test') {
 	'147-161': {
 	/*
 		if ( ONLY_RUN_TESTS == 'false' ) {
-			timeout(time:1, unit:'HOURS') {
+			timeout(time:2, unit:'HOURS') {
 				try{
 					//'Install Intellego DPE on Node 147'
 					node('10.0.158.147') {
@@ -444,10 +455,11 @@ stage ('Reporting') {
 			try{
 				//emailext attachmentsPattern: '*.log, *.html', body: 'BUILD_URL = ' + env.BUILD_URL, subject: 'END Intellego Coded Pipeline Build SOURCE - ' + INTELLEGO_CODE_BRANCH + ' RESTAPI - ' + RESTAPI_BRANCH, to: MAILING_LIST
 				emailext attachmentsPattern: '*.log, *.html', mimeType: 'text/html', body: '${FILE,path="' + TMPDIR + '/Summary.HTML"}', subject: 'END Intellego Coded Pipeline Build SOURCE - ' + INTELLEGO_CODE_BRANCH + ' RESTAPI - ' + RESTAPI_BRANCH, to: MAILING_LIST
-				sh 'touch ' + INTELLEGO_CODE_BRANCH + '-' + RESTAPI_BRANCH + '.build'          
+				//sh 'touch ' + INTELLEGO_CODE_BRANCH + '-' + RESTAPI_BRANCH + '.build'          
 				//sh 'rm -rf ' + TMPDIR
 				sh 'zip -j Failed_Test_Results.zip ' + TMPDIR + '/*'
 				archive 'Failed_Test_Results.zip, Summary.HTML, *.build'
+				//sh 'echo No. of Tests failed: ' + TESTS_FAILED
 			}
 			catch(err){
 				currentBuild.result = 'SUCCESS'
