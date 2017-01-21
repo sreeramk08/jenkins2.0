cf87b61a2fbf90578ad917a85168079bd6966ea2
SKrishna <sreeramk@hotmail.com>
2017-01-20 20:50:42 -0800

    Works for Intellego. 2.5.9

--- iso-creation-pipeline.groovy
+++ iso-creation-pipeline.groovy
@@ -1,11 +1,116 @@
 #!groovy
 
+currentBuild.displayName = PROJECT + '-' + VERSION
+
+if (RPM_SYNC == 'true' ) {
+
+	stage ('Sync RPMs') {
+	
+		if (PLATFORM == 'RHEL-6' ) {
+			node ('rhel6-iso-build-machine') {
+				update_rhel6_repo()
+			}
+		}
+		else if ( PLATFORM == 'RHEL-7' ) {
+			node ('rhel7-repo') {
+				update_rhel7_repo()
+			}
+		}
+	}
+}
+
+stage ('Build ISO') {
+
+	if (PLATFORM == 'RHEL-6' ) {
+	
+		node ('rhel6-iso-build-machine') {		
+			ws('/home/iso-build-user/ISO_BUILD'){
+				git url: 'ssh://git@10.0.135.6/platform.git', branch: 'master'
+			}
+					
+			def DIR = '/home/iso-build-user/ISO_BUILD/platform-isos/' + PLATFORM + '/' + PROJECT
+
+			ws("${DIR}") {
+				
+					sh(script: "sudo gmake nuke; sudo gmake iso VERSION=${VERSION}")
+					def exists = sh(script: "ls dist/*.iso ", returnStdout:true)
+					echo "Got exists as: " + exists
+								
+					// Iso creation was successful
+					if (exists) {
+					    echo "ISO's are available! Renaming them..."
+						def ISO = sh(script:"cd dist; ls *.iso | grep -v upgrade", returnStdout: true).trim()
+						sh 'sudo mv dist/' + ISO + ' dist/intellego-spl-new-' + VERSION + '.iso'
+						def UP_ISO = sh(script:"cd dist; ls *.iso | grep upgrade", returnStdout: true).trim()
+						sh 'sudo mv dist/' + UP_ISO + ' dist/intellego-spl-new-' + VERSION + '-upgrade.iso'
+						echo "Copying to ISO Datastore"
+						sh 'scp dist/*.iso isoadmin@10.0.155.54:/ISOFolder/SPL'
+						emailext body: 'ISO Creation successful!', attachLog: true, subject: 'ISO Creation Completed', to: MAILING_LIST
+					} 
+					else {
+						
+						emailext body: 'ISO Creation unsuccessful!', attachLog: true, subject: 'ISO Creation unSuccessfull', to: MAILING_LIST
+						currentBuild.result = 'FAILURE'
+						throw err
+					}
+			} // ws block
+		} // end of node
+	
+	}
+	else (PLATFORM == 'RHEL-7' ) {
+	
+		node ('rhel7-iso'){
+			ws('/home/iso-build-user/ISO_BUILD'){
+				git url: 'ssh://git@10.0.135.6/platform.git', branch: 'master'
+			}
+					
+			def DIR = '/home/iso-build-user/ISO_BUILD/platform-isos/' + PLATFORM + '/' + PROJECT
+
+			ws("${DIR}") {
+				if ( PROJECT == 'sensor' ){
+					// Only for sensor the ask is to append a rhel7 next to the version
+					sh(script: "sudo gmake nuke; sudo gmake iso RELEASE=${VERSION}")
+				}
+				else {
+					sh(script: "sudo gmake nuke; sudo gmake iso VERSION=${VERSION}")
+				}
+				def exists = sh(script: "ls dist/*.iso ", returnStdout:true)
+				echo "Got exists as: " + exists
+								
+				// Iso creation was successful
+				if (exists) {
+				    echo "ISO's are available! Renaming them..."
+					def ISO = sh(script:"cd dist; ls *.iso | grep -v upgrade", returnStdout: true).trim()
+					sh 'sudo mv dist/' + ISO + ' dist/intellego-spl-new-' + VERSION + '.iso'
+					def UP_ISO = sh(script:"cd dist; ls *.iso | grep upgrade", returnStdout: true).trim()
+					sh 'sudo mv dist/' + UP_ISO + ' dist/intellego-spl-new-' + VERSION + '-upgrade.iso'
+					echo "Copying to ISO Datastore"
+					sh 'scp dist/*.iso isoadmin@10.0.155.54:/ISOFolder/SPL'
+					emailext body: 'ISO Creation successful!', attachLog: true, subject: 'ISO Creation Completed', to: MAILING_LIST
+				} 
+				else {
+						
+					emailext body: 'ISO Creation unsuccessful!', attachLog: true, subject: 'ISO Creation unSuccessfull', to: MAILING_LIST
+					currentBuild.result = 'FAILURE'
+					throw err
+				}
+			}
+		}
+
+	}
+
+
+}
+
+
+
+//Methods
+
 def update_rhel7_repo() {
 	echo "Running a repo sync with Redhat 7"
 	ws('/home/iso-build-user/scripts'){
-		// Try to register with redhat first
 		try { 
-			sh 'sudo  ./unregister.sh' //force unregister first
+			sh 'sudo  ./unregister.sh; ' //force unregister first
 			sh 'sudo  ./subscribe.sh | tee /tmp/aa '  // then try to subscribe
 			//ERROR = sh(script: "grep 'No subscriptions are available' /tmp/aa ; echo \$?", returnStdout: true).trim()
 			//if ( ERROR == '0' ) {
@@ -15,8 +120,7 @@ def update_rhel7_repo() {
 			//}
 		}
 		catch(err){ // If registration failed, unregister and re-register
-			
-			emailext body: 'Reposync for RHEL-7 failed ', attachLog: true, subject: 'Reposync for RHEL-7 failed during subscription', to: MAILING_LIST
+			emailext body: 'Subscritption for RHEL-7 failed ', attachLog: true, subject: 'Reposync for RHEL-7 failed during subscription', to: MAILING_LIST
 		}
 		// Try to sync
 		try { // sync repo
@@ -24,7 +128,6 @@ def update_rhel7_repo() {
 		}
 		catch (err) {
 			currentBuild.result = 'FAILURE'
-			sh 'sudo  ./unregister.sh' // unregister before exiting
 			sh 'sudo ./unregister.sh'
 			emailext body: 'Reposync for RHEL-7 failed ', attachLog: true, subject: 'Reposync for RHEL-7 failed', to: MAILING_LIST
 			throw err
@@ -38,83 +141,18 @@ def update_rhel7_repo() {
 
 def update_rhel6_repo() {
 	echo "Running a repo sync with Redhat 6"
-    try { // Try to register with redhat first
-        sh 'sudo  /home/victorhugo/repos/subscribe.sh'
-    }
-    catch(err){ // If registration failed, unregister and re-register
-		sh 'sudo  /home/victorhugo/repos/unregister.sh'
-        sh 'sudo  /home/victorhugo/repos/subscribe.sh'
-    }
-    // Try to sync
-    try { 
-		sh 'sudo  /home/victorhugo/repos/repoSync-NG.sh'
-    }
-    catch (err) {
-		currentBuild.result = 'FAILURE'
-        sh 'sudo /home/victorhugo/repos/unregister.sh' // unregister before exiting
-        emailext body: 'Reposync for RHEL-6 failed ', attachLog: true, subject: 'Reposync for RHEL-6 failed', to: MAILING_LIST
-		throw err
-    }
-    // if we got here the repo sync was successful
-    sh 'sudo  /home/victorhugo/repos/clean.sh' // Cleanup
-    sh 'sudo /home/victorhugo/repos/repoFix.sh' //  Fix issues in repo
-    // Unsubscribe once done
-    sh 'sudo  /home/victorhugo/repos/unregister.sh'
+	ws('/home/iso-build-user/scripts'){
+		try { 
+			sh 'sudo  ./unregister.sh ; sudo ./subscribe.sh ' 
+		}
+		catch(err){ 
+			emailext body: 'Subscritption for RHEL-6 failed ', attachLog: true, subject: 'Subscritption for RHEL-6 failed', to: MAILING_LIST
+		}
+		// Try to sync
+		
+		sh 'sudo  ./repoSync-NG.sh'
+		
+		// if we got here the repo sync was successful
+		sh 'sudo  ./clean.sh ; sudo ./repoFix.sh ; sudo  ./unregister.sh' // Cleanup
+	}
 }
-
-stage ('rpm sync') {
-
-    node {
-        
-            echo "Running rpm sync..."
-            
-            if (PLATFORM == 'RHEL-6' ) {
-				if (RPM_SYNC == 'true' ) {
-					node ('rhel6-iso-build-machine') {
-						update_rhel6_repo()
-					} // end of node
-				}
-            } // end of rhel6 block
-            
-            if ( PLATFORM == 'RHEL-7' ) {
-				if ( RPM_SYNC == 'true' ) {
-					node ('rhel7-repo') {
-						update_rhel7_repo()
-					}
-				}
-				node ('rhel7-iso'){
-					ws('/home/iso-build-user/ISO_BUILD'){
-						git url: 'ssh://git@10.0.135.6/platform.git', branch: 'master'
-					}
-					
-					def DIR = '/home/iso-build-user/ISO_BUILD/platform-isos/' + PLATFORM + '/' + PROJECT
-
-					ws("${DIR}") {
-						try {
-							if ( PROJECT == 'sensor' ){
-								// Only for sensor the ask is to append a rhel7 next to the version
-								sh(script: "sudo gmake nuke; sudo gmake iso RELEASE=${VERSION}")
-							}
-							else {
-								sh(script: "sudo gmake nuke; sudo gmake iso VERSION=${VERSION}")
-							}
-						}
-						catch(err){
-							emailext body: 'ISO Creation unsuccessful!', attachLog: true, subject: 'ISO Creation unSuccessfull', to: MAILING_LIST
-							currentBuild.result = 'FAILURE'
-						}
-					} // ws block
-				}
-            } // end of rhel7 block
-   
-    } // end of node
-} // end of rpm sync stage
-
-
-
-// Email success
-node {
-	  emailext body: 'ISO Creation successful!', attachLog: true, subject: 'ISO Creation Successfull', to: MAILING_LIST
-} 
-
-
